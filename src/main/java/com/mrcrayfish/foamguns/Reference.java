8507566aa2edb404520e67367f169cadf29412dd
MrCrayfish <mrcrayfish@hotmail.com>
2019-08-24 14:44:31 +0930

    Fixed dependency version

--- src/main/java/com/mrcrayfish/foamguns/Reference.java
+++ src/main/java/com/mrcrayfish/foamguns/Reference.java
@@ -9,7 +9,7 @@ public class Reference
     public static final String MOD_NAME = "Foam Guns";
     public static final String MOD_VERSION = "0.1.0";
     public static final String MC_VERSION = "[1.12.2]";
-    public static final String DEPENDENCIES = "required-after:cgm@[0.13.0,);";
+    public static final String DEPENDENCIES = "required-after:cgm@[0.13.1,);";
     public static final String PROXY_CLIENT = "com.mrcrayfish.foamguns.proxy.ClientProxy";
     public static final String PROXY_SERVER = "com.mrcrayfish.foamguns.proxy.CommonProxy";
 }
